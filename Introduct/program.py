d7d626b78d70797fbc4d3f76afeba4383c350c54
Antonio Lima Valente <antonio.valente2023@proton.me>
2023-08-09 10:59:50 -0300

    Version 1.0

---
+++ Introduct/program.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+ip = str(input("Digite o IP: "))
+porta = int(input("Digite a porta: "))
+ver = 1.1
+
+
+print("Scan versão: %.1f \nVarrendo host: %s na porta: %d" %(ver,ip,porta))
+
