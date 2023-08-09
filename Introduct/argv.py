cf8dca683a6d2c9520417cd918c7b87cdba93253
Antonio Lima Valente <antonio.valente2023@proton.me>
2023-08-09 11:14:55 -0300

    Versão final

--- Introduct/argv.py
+++ Introduct/argv.py
@@ -6,5 +6,17 @@ import os #comandos do sistema operacional
 #Exemplo: ./argv.py 192.168.0.1 80
 print("Varrendo o host: ",sys.argv[1], "Na porta: ",sys.argv[2])
 
+#Exemplo de laço de repetição:
+ip = 0
+for ip in range(1,255):
+    print ("Varrendo IP: 192.168.0.%s" %ip)
+
+#Trabalhando com condições:
+if len(sys.argv) <= 2:
+        print("Modo de uso: ./argv.py IP PORTA")
+
+else:   
+    print("Varrendo o host: ",sys.argv[1], "Na porta: ",sys.argv[2])
+    
 #O comando chama a função do sistema operacional
 os.system("netstat -nlpt")
